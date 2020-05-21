77327e8ce8d00c89d7a941e62610d31878a979a6
Digvijaysing <IM-Shadow@users.noreply.github.com>
2020-05-21 22:50:48 +0530

    Updated Constraints

--- app/src/main/java/com/techcorner/shopseeker/MainActivity.java
+++ app/src/main/java/com/techcorner/shopseeker/MainActivity.java
@@ -6,12 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.ibm.cloud.appid.android.api.AppID;
+import com.ibm.cloud.appid.android.api.AuthorizationException;
+import com.ibm.cloud.appid.android.api.AuthorizationListener;
+import com.ibm.cloud.appid.android.api.LoginWidget;
+import com.ibm.cloud.appid.android.api.tokens.AccessToken;
+import com.ibm.cloud.appid.android.api.tokens.IdentityToken;
+import com.ibm.cloud.appid.android.api.tokens.RefreshToken;
 import com.techcorner.shopseeker.ui.login.clogin;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button Btnmove;
+
+
+    private Button Button;
     private Button Btnnmove;
 
 
@@ -20,15 +30,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Btnmove = findViewById(R.id.main_login);
+        Button = (Button) findViewById(R.id.main_login);
 
-        Btnmove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                moveCtologin();
-            }
-        });
+        AppID.getInstance().initialize(getApplicationContext(), "5deb32e2-2640-4924-b237-9b92bf99145f", AppID.REGION_UK);
 
         Btnnmove = findViewById(R.id.main_create);
 
@@ -43,14 +47,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void moveCtocreate(){
-     Intent intent=new Intent(MainActivity.this, cacnt.class);
-     startActivity(intent);
+    public void moveCtocreate(){
+        LoginWidget loginWidget = AppID.getInstance().getLoginWidget();
+        loginWidget.launchSignUp(this, new AuthorizationListener() {
+            @Override
+            public void onAuthorizationFailure (AuthorizationException exception) {
+                //Exception occurred
+            }
+
+            @Override
+            public void onAuthorizationCanceled () {
+                //Sign up canceled by the user
+            }
+
+            @Override
+            public void onAuthorizationSuccess (AccessToken accessToken, IdentityToken identityToken, RefreshToken refreshToken) {
+                if (accessToken != null && identityToken != null) {
+                    //User authenticated
+                    Intent intent=new Intent(MainActivity.this, homepage.class);
+                    startActivity(intent);
+
+
+                } else {
+                    //email verification is required
+                }
+            }
+        });
     }
 
-    private void moveCtologin(){
-        Intent intent=new Intent(MainActivity.this, clogin.class);
-        startActivity(intent);
-    }
 
+    public void onclickevent(View view) {
+        LoginWidget loginWidget = AppID.getInstance().getLoginWidget();
+        loginWidget.launch(this, new AuthorizationListener() {
+            @Override
+            public void onAuthorizationFailure (AuthorizationException exception) {
+                //Exception occurred
+            }
+
+            @Override
+            public void onAuthorizationCanceled () {
+                //Authentication canceled by the user
+            }
+
+            @Override
+            public void onAuthorizationSuccess (AccessToken accessToken, IdentityToken identityToken, RefreshToken refreshToken) {
+                //User authenticated
+                Intent intent=new Intent(MainActivity.this, homepage.class);
+                startActivity(intent);
+
+            }
+        });
+    }
 }
